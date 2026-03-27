9cf2dd39c51b5c3f2ecfaebeff36dd187815c6e7
naveenkhatri1008 <naveenkhatri1008@gmail.com>
2026-03-27 16:51:11 +0530

    Update Bgardhdiv.java

--- Bgardhdiv.java
+++ Bgardhdiv.java
@@ -74,7 +74,7 @@ public class Bgardhdiv {
 		try (Playwright playwright = Playwright.create()) {
 
 			Browser browser = playwright.chromium().launch(
-					new BrowserType.LaunchOptions().setHeadless(false)
+					new BrowserType.LaunchOptions().setHeadless(true)
 					);
 
 			Page page = browser.newPage();
