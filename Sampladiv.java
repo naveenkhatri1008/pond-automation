60294faa1b8a255580564f17a94ad27712d945bd
naveenkhatri1008 <naveenkhatri1008@gmail.com>
2026-03-27 16:50:45 +0530

    Update Sampladiv.java

--- Sampladiv.java
+++ Sampladiv.java
@@ -64,7 +64,7 @@ public class Sampladiv {
         try (Playwright playwright = Playwright.create()) {
 
             Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false)
+                    new BrowserType.LaunchOptions().setHeadless(true)
             );
 
             Page page = browser.newPage();
